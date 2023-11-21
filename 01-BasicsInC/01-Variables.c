b2b9755b67be57801dc6410a762a974687488352
Karuppasamy K <karuppan.the.pentester@gmail.com>
2023-11-22 02:20:16 +0530

    01-Variables

---
+++ 01-BasicsInC/01-Variables.c
@@ -0,0 +1,10 @@
+// Importing the header files
+#include <stdio.h>
+
+// Creating the main function
+void main(){
+
+    // Defining variable in C needs to mention the type of the variable
+    int a = 10;
+
+}
