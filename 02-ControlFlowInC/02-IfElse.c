6fa62d938069a0f89d1ed27e2924f2d97060066b
Karuppasamy K <karuppan.the.pentester@gmail.com>
2023-11-23 23:50:26 +0530

    02-IfElse

---
+++ 02-ControlFlowInC/02-IfElse.c
@@ -0,0 +1,31 @@
+// Importing the header files
+#include <stdio.h>
+
+// Creating the main function
+int main(){
+
+    // Defining a variable for storing the input
+    int points;
+
+    // Creating label for input
+    printf("Enter your point: ");
+
+    // Storing the input to the variable
+    scanf("%d",&points);
+
+    // Checking whether the point is more than 5 or not
+    if (points >= 5 ){
+
+        // Displaying the output
+        printf("Your mark '%d' is more than 4\n",points);
+    }
+
+    // Creating an else block
+    else {
+
+        // Displaying the output
+        printf("Your mark '%d' is less than 5\n",points);
+    }
+
+    return 0;
+}
