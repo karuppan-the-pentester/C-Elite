1e01dbe3840be30b306353c53fc694c48f675c12
Karuppasamy K <karuppan.the.pentester@gmail.com>
2023-11-23 19:00:48 +0530

    02-Datatypes

---
+++ 01-BasicsInC/02-DataTypes.c
@@ -0,0 +1,25 @@
+// Importing the header files
+#include <stdio.h>
+
+// Creating the main function
+void main(){
+
+    // Memory size of Int is 4
+    int a = 10;
+
+    // Memory size of Float is 4
+    float b = 12.4;
+
+    // Memory size of Double is 8
+    double c = 3.1426583;
+
+    // Memory size of Char is 1
+    char d = 'K';
+
+    // Memory value of Long is 4 or 8
+    long e = 123456789;
+
+    // Memory value of Short is 2
+    short f = 231;
+
+}
