cc9ab2e2b4a7ad69b3bede1746e30d282f2c9f3f
Karuppasamy K <karuppan.the.pentester@gmail.com>
2023-11-23 23:37:16 +0530

    03-Inputs&Outputs

---
+++ 01-BasicsInC/03-Inputs&Outputs.c
@@ -0,0 +1,20 @@
+// Importing the header files
+#include <stdio.h>
+
+// Creating the main function
+int main(){
+
+    // Defining a variable for storing the input
+    int n;
+
+    // Creating label for input
+    printf("Enter the value of n: ");
+
+    // Storing the input to the variable
+    scanf("%d",&n);
+
+    // Displaying the input
+    printf("The value of n is : %d\n",n);
+
+    return 0;
+}
