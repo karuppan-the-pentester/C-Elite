d8faf63b727ac6b5710f3733ed2ddec5315748a9
Karuppasamy K <karuppan.the.pentester@gmail.com>
2023-11-24 22:03:29 +0530

    07-DoWhileLoop

---
+++ 02-ControlFlowInC/07-DoWhileLoop.c
@@ -0,0 +1,23 @@
+// Importing the header files
+#include <stdio.h>
+
+// Creating the main function
+int main(){
+
+    // Initializing the variable for the condition
+    int i = 0;
+
+    // Looping using DoLoop
+    do {
+
+        //printing the looping varible
+        printf("%d\n",i);
+
+        // Incrementing the variable
+        i++;
+
+    }while ( i < 10 ); // Defining condition
+
+
+    return 0;
+}
