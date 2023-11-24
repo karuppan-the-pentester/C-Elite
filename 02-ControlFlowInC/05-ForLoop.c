c808c24d635b21f1f412587b8b905a538eb9765b
Karuppasamy K <karuppan.the.pentester@gmail.com>
2023-11-24 09:46:41 +0530

    05-ForLoop

---
+++ 02-ControlFlowInC/05-ForLoop.c
@@ -0,0 +1,13 @@
+// Importing the header files
+#include <stdio.h>
+
+// Creating the main function
+int main(){
+
+    // Looping using forloop
+    for (int i = 0; i < 5; ++i) {
+        printf("%d \n",i);
+    }
+
+    return 0;
+}
