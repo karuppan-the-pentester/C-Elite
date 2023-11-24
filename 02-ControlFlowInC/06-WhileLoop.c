9888258881bf6d37261423a168ee53665631d009
Karuppasamy K <karuppan.the.pentester@gmail.com>
2023-11-24 22:01:10 +0530

    06-WhileLoop

---
+++ 02-ControlFlowInC/06-WhileLoop.c
@@ -0,0 +1,22 @@
+// Importing the header files
+#include <stdio.h>
+
+// Creating the main function
+int main(){
+
+    // Initializing the variable for the condition
+    int i = 0;
+
+    // Looping using WhileLoop
+    while ( i < 10 ){  // Defining condition
+
+        //printing the looping varible
+        printf("%d\n",i);
+
+        i++;
+
+    }
+
+
+    return 0;
+}
