93bd474757cd94247575888d26f95259620cd051
Karuppasamy K <karuppan.the.pentester@gmail.com>
2023-11-29 15:31:01 +0530

    00-CollegeProgram

--- 00-CollegeCodes/00-Test.c
+++ 00-CollegeCodes/00-Test.c
@@ -24,7 +24,6 @@ int Palindrome() {
     }
 }
 
-#include <stdio.h>
 
 void Break(){
 
@@ -126,7 +125,49 @@ void OnseStar(){
             printf("*");
         }
 
+        printf("\n");
 
+    }
+}
+
+void HalfStar(){
+
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+
+
+    for(int i=0;i<n;i++){
+
+        printf("\t\t\t\t\t");
+
+        for (int j = 0; j < n-i ; ++j) {
+            printf(" ");
+
+        }
+
+        for (int j = 0;  j < (i)+1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+
+
+    }
+
+
+    for(int i=0;i<n-1;i++){
+
+        printf("\t\t\t\t\t");
+
+        for (int j = n; j > n-i ; j--) {
+            printf(" ");
+
+        }
+
+        for (int j = (2*n)+1;  j > n+i; j--) {
+            printf("*");
+        }
 
         printf("\n");
 
@@ -135,7 +176,7 @@ void OnseStar(){
 
 void main(){
 
-    OnseStar();
+    HalfStar();
 
 }
 
