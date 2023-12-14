fdd039769caf29dfd4258ddda7ac90ee3fd269cb
Karuppasamy K <karuppan.the.pentester@gmail.com>
2023-12-15 01:12:17 +0530

    01-BestTimeToBuyAndSell.c

--- 00-CollegeCodes/01-CodingNinjas/01-BestTimeToBuyAndSell.c
+++ 00-CollegeCodes/01-CodingNinjas/01-BestTimeToBuyAndSell.c
@@ -5,16 +5,20 @@ void main() {
     int NoOfElements,Prices[200],OutPut;
     int MaxPrice,MinPrice,MaxIndex=0,MinIndex=0;
 
-//    printf("Enter the Number of Elements :");
+    // Asking Number of variables
+    // printf("Enter the Number of Elements :");
     scanf("%d",&NoOfElements);
 
-//    printf("Enter the price OneByOne :");
+    // Asking the Elements
+    // printf("Enter the price OneByOne :");
     for (int i = 0; i < NoOfElements; ++i) {
         scanf("%d",&Prices[i]);
     }
 
+    // Looping For increase the Max Value
     for (int j = 0; j < NoOfElements; ++j) {
 
+        // Finding Maximum Value
         MaxPrice = Prices[j];
         for (int i = j; i < NoOfElements; ++i) {
             if (MaxPrice < Prices[i]) {
@@ -23,12 +27,14 @@ void main() {
             }
         }
 
+        // Finding Minimum Value
         MinPrice = Prices[0];
         for (int i = 0; i < NoOfElements; ++i) {
             if (MinPrice > Prices[i]) {
                 MinPrice = Prices[i];
                 MinIndex = i;
 
+                // Check the Condition by index Value of Max & Min Value
                 if( MaxIndex >= MinIndex ){
                     OutPut = MaxPrice - MinPrice;
                 } else {
@@ -37,17 +43,13 @@ void main() {
             }
         }
 
-    //    printf("%d",OutPut);
-
+        // If the Value Occurs Break the Loop
         if (OutPut != (int) NULL){
-            printf("%d",OutPut);
             break;
         }
-//        else {
-//            printf("0");
-//        }
     }
 
+    // Displaying the Output
     if (OutPut != (int) NULL){
         printf("%d",OutPut);
     }
